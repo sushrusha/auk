8791b6fb5e7a53163a4f5858f9feb515d63292c3
sushrusha <53030121+sushrusha@users.noreply.github.com>
2019-07-18 10:34:06 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,295 @@
+<?php include 'menu.html';?>
+<!-- Main img -->
+<div class="main_img">
+    <img class="main_img" src="./img/photo.png" style="width: 100%">
+</div>
+<!-- Yellow button -->
+<div>
+</div>
+<!-- Why section -->
+<div class="why">
+      <center> <img  src="./img/yellow bird.png"> </center>
+      <div><center class="heading1">WHY US</center></div>
+    <div class="features">
+      <div class="row" >
+        <div class="col-md-1 cl-lg-1">
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-2 cl-lg-2 mb-tb-10">
+            <div class="frame_squre">
+             <center><img src ="img/free listing icon.png" style="height: 80px; width: 80px"></center>
+             <center> <img class="heading-yellow-img" src="./img/log.png" ></center>
+             <h4 class="feature_txt">Free Listing</h4>
+            </div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-2 cl-lg-2 mb-tb-10">
+            <div class="frame_squre">
+                    <center><img src ="img/free listing icon.png" style="height: 80px; width: 80px"></center>
+                    <center> <img class="heading-yellow-img" src="./img/log.png"></center>  
+                    <h4 class="feature_txt">Verifed Listing</h4>
+            </div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-2 cl-lg-2 mb-tb-10">
+            <div class="frame_squre">
+                    <center><img src ="img/free listing icon.png" style="height: 80px; width: 80px"></center>
+                    <center> <img class="heading-yellow-img" src="./img/log.png"></center>     
+                    <h4 class="feature_txt">Hassle Free Search</h4>
+            </div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-2 cl-lg-2 mb-tb-10">
+            <div class="frame_squre">
+                    <center><img src ="img/free listing icon.png" style="height: 80px; width: 80px"></center>
+                    <center> <img class="heading-yellow-img" src="./img/log.png"></center>  
+                    <h4 class="feature_txt">Free For All</h4>   
+            </div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-2 cl-lg-2 mb-tb-10">
+            <div class="frame_squre">
+                    <center><img src ="img/free listing icon.png" style="height: 80px; width: 80px"></center>
+                    <center> <img class="heading-yellow-img" src="./img/log.png"></center>  
+                    <h4 class="feature_txt">No Brokers</h4> 
+            </div>
+        </div>     
+        <div class="col-md-1 cl-lg-1 mb-tb-10">
+        </div>                          
+       </div>
+    </div>
+    </div>
+     <!-- End of Why section -->
+     <!-- Aucknest Originals -->
+<div class="original">
+    <center><img src="./img/blue 200 into 70.png"></center>
+    <div><center class="heading2">AUKNEST ORIGINALS</center></div>
+    
+        <div class="row" style="margin: auto">
+
+             <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 pd-2">
+                <div class="img-view-txt">
+                <center><img class="img-width-100" src="img/320 into 370.jpg"></center>
+                <center><span class="txt-view-more">VIEW MORE DETAILS</span></center>
+                </div>
+             </div>
+             <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 pd-2">
+                <div class="img-view-txt">
+                    <center><img class="img-width-100" src="img/320 into 370.jpg"></center>
+                    <center><span class="txt-view-more">VIEW MORE DETAILS</span></center>
+                </div>
+             </div>
+              <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 pd-2">
+                  <div class="img-view-txt">
+                      <center><img class="img-width-100" src="img/320 into 370.jpg"></center>
+                      <center><span class="txt-view-more">VIEW MORE DETAILS</span></center>
+                  </div>
+              </div>
+              <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 pd-2">
+                  <div class="img-view-txt">
+                      <center><img class="img-width-100" src="img/320 into 370.jpg"></center>
+                      <center><span class="txt-view-more">VIEW MORE DETAILS</span></center>
+                  </div>
+              </div>
+              <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 pd-2">
+                  <div class="img-view-txt">
+                      <center><img class="img-width-100" src="img/320 into 370.jpg"></center>
+                      <center><span class="txt-view-more">VIEW MORE DETAILS</span></center>
+                  </div>
+              </div>
+              <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 pd-2">
+                  <div class="img-view-txt">
+                      <center><img class="img-width-100" src="img/320 into 370.jpg"></center>
+                      <center><span class="txt-view-more">VIEW MORE DETAILS</span></center>
+                  </div>
+              </div>
+              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 pd-2">
+              <center><button class="btn-viewMore"><b>VIEW ALL AUKNEST ORIGINAL</b></button></center>
+              </div>
+        </div>
+    
+</div>
+<!-- Featured Property -->
+<div class="featured-block">                                          
+        <center> <img  src="./img/yellow bird.png"> </center>
+      <div><center class="heading1">FEATURED PROPERTY</center></div>
+      <div class="row" style="margin: auto">
+
+          <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 pd-2">
+             <div class="img-view-txt">
+             <center><img class="img-width-100" src="img/320 into 370.jpg"></center>
+             <center><span class="txt-view-more">VIEW MORE DETAILS</span></center>
+             </div>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 pd-2">
+             <div class="img-view-txt">
+                 <center><img class="img-width-100" src="img/320 into 370.jpg"></center>
+                 <center><span class="txt-view-more">VIEW MORE DETAILS</span></center>
+             </div>
+          </div>
+           <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 pd-2">
+               <div class="img-view-txt">
+                   <center><img class="img-width-100" src="img/320 into 370.jpg"></center>
+                   <center><span class="txt-view-more">VIEW MORE DETAILS</span></center>
+               </div>
+           </div>
+           <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 pd-2">
+               <div class="img-view-txt">
+                   <center><img class="img-width-100" src="img/320 into 370.jpg"></center>
+                   <center><span class="txt-view-more">VIEW MORE DETAILS</span></center>
+               </div>
+           </div>
+           <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 pd-2">
+               <div class="img-view-txt">
+                   <center><img class="img-width-100" src="img/320 into 370.jpg"></center>
+                   <center><span class="txt-view-more">VIEW MORE DETAILS</span></center>
+               </div>
+           </div>
+           <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 pd-2">
+               <div class="img-view-txt">
+                   <center><img class="img-width-100" src="img/320 into 370.jpg"></center>
+                   <center><span class="txt-view-more">VIEW MORE DETAILS</span></center>
+               </div>
+           </div>
+           <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 pd-2">
+           <center><button class="btn-viewMore"><b>VIEW ALL AUKNEST ORIGINAL</b></button></center>
+           </div>
+     </div>
+</div>
+
+<!-- Auknest bring you more -->
+<div class="Bring-block">
+        <center> <img  src="./img/yellow bird.png"> </center>
+        <div><center class="heading1">AUKNEST BRING YOU MORE</center></div>  
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                    <img src="img/Vardhman-Springdale-project-image.jpg" style="height:300px; width:100%">
+                    <div style="height: 100px;">
+                      <center><h4><b>Looking to RentOut / Sale Your Entire Building / Floor</b></h4></center>
+                      <center><button class="btn-enquiry"><b>ENQUIRE NOW</b></button></center>
+
+                    </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            <img src="img/83840a84-2f67-4924-ac58-22d736c86712.png" style="height:400px; width:100%">
+            </div>
+        </div>
+</div>
+<!-- Advertisement Block -->
+<div class="advertisement">
+    <div class="row">
+        <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
+          <!-- <img src="img/83840a84-2f67-4924-ac58-22d736c86712.png"> -->
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4 heading1">
+              DOWNLOAD OUR APP
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4" >
+                <img src="img/81679.png">
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
+            
+        </div>
+    </div>
+</div>
+
+<!-- Testimonials -->
+<div class="testimonial">
+    <center><img src="./img/blue 200 into 70.png"></center>
+    <div><center class="heading2">TESTIMONIALS</center></div>
+    <div class="row">
+        <div class="col-md-12">
+            <div id="testimonial-slider" class="owl-carousel">
+               
+                <div class="testimonial-item equal-height style-6" style="height: 254px;">
+                    <div class="">
+                        <img class="mb-20" style="border-radius:50%;" src="http://tivatheme.com/wordpress/wp-content/plugins/tiva-testimonials-slider/images/agnes.jpg" alt="Agnes A. Bell">
+                    </div>
+                    <div class="cell-right">
+                        <div class="testimonial-name">Agnes A. Bell</div>
+                        <div >
+                                <hr class="hr-white-line">
+                                <div class="testimonial-content quote">
+                                        This is one of the best WordPress themes I have used. All aspects are exactly what experienced designers and developers crave from a theme.
+                                </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="testimonial-item equal-height style-6" style="height: 254px;">
+                    <div class="testimonial-image cell-left">
+                        <img  class="mb-20" style="border-radius:50%" src="http://tivatheme.com/wordpress/wp-content/plugins/tiva-testimonials-slider/images/agnes.jpg" alt="Agnes A. Bell">
+                    </div>
+                    <div class="cell-right">
+                            <div class="testimonial-name">Agnes A. Bell</div>
+                            <div >
+                                    <hr class="hr-white-line">
+                                    <div class="testimonial-content quote">
+                                            This is one of the best WordPress themes I have used. All aspects are exactly what experienced designers and developers crave from a theme.
+                                    </div>
+                            </div>
+                        </div>
+                </div>
+                <div class="testimonial-item equal-height style-6" style="height: 254px;">
+                        <div class="testimonial-image cell-left">
+                            <img  class="mb-20" style="border-radius:50%" src="http://tivatheme.com/wordpress/wp-content/plugins/tiva-testimonials-slider/images/agnes.jpg" alt="Agnes A. Bell">
+                        </div>
+                        <div class="cell-right">
+                                <div class="testimonial-name">Agnes A. Bell</div>
+                                <div >
+                                        <hr class="hr-white-line">
+                                        <div class="testimonial-content quote">
+                                                This is one of the best WordPress themes I have used. All aspects are exactly what experienced designers and developers crave from a theme.
+                                        </div>
+                                </div>
+                            </div>
+                </div>
+                <div class="testimonial-item equal-height style-6" style="height: 254px;">
+                        <div class="testimonial-image cell-left">
+                            <img  class="mb-20" style="border-radius:50%" src="http://tivatheme.com/wordpress/wp-content/plugins/tiva-testimonials-slider/images/agnes.jpg" alt="Agnes A. Bell">
+                        </div>
+                        <div class="cell-right">
+                                <div class="testimonial-name">Agnes A. Bell</div>
+                                <div >
+                                        <hr class="hr-white-line">
+                                        <div class="testimonial-content quote">
+                                                This is one of the best WordPress themes I have used. All aspects are exactly what experienced designers and developers crave from a theme.
+                                        </div>
+                                </div>
+                            </div>
+                    </div>
+                    <div class="testimonial-item equal-height style-6" style="height: 254px;">
+                            <div class="testimonial-image cell-left">
+                                <img  class="mb-20" style="border-radius:50%" src="http://tivatheme.com/wordpress/wp-content/plugins/tiva-testimonials-slider/images/agnes.jpg" alt="Agnes A. Bell">
+                            </div>
+                            <div class="cell-right">
+                                    <div class="testimonial-name">Agnes A. Bell</div>
+                                    <div >
+                                            <hr class="hr-white-line">
+                                            <div class="testimonial-content quote">
+                                                    This is one of the best WordPress themes I have used. All aspects are exactly what experienced designers and developers crave from a theme.
+                                            </div>
+                                    </div>
+                                </div>
+                        </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+$(document).ready(function(){
+  $("#testimonial-slider").owlCarousel({
+      items:1,
+      itemsDesktop:[1000,1],
+      itemsDesktopSmall:[980,1],
+      itemsTablet:[768,1],
+      pagination:true,
+      navigation:false,
+      navigationText:["",""],
+      autoPlay:true
+  });
+});
+</script>
+
+        
+ 
+<!-- End testimonial -->
+
+</div>
+</div>
+</div>
+
